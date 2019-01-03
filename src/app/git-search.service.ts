eba2d700802f2d9d5fcc25de47db84010f11a076
Niall Riordan <nriordan.dev@gmail.com>
2019-01-03 11:37:03 +0100

    Add caching to GitSearchService

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -2,17 +2,31 @@ import { GitSearch } from './git-search';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { publishReplay, refCount } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
+  search: Observable<GitSearch>;
+  cachedValue: string;
   constructor(private http: HttpClient) {
 
   }
 
-  gitSearch = (query: string): Observable<GitSearch> => {
-    const url = `https://api.github.com/search/repositories?q=${query}`;
-    return this.http.get<GitSearch>(url);
+  gitSearch: Function = (query: string): Observable<GitSearch> => {
+    if (!this.search) {
+      const url = `https://api.github.com/search/repositories?q=${query}`;
+      this.search = this.http.get<GitSearch>(url)
+        .pipe(
+          publishReplay(1),
+          refCount()
+        );
+      this.cachedValue = query;
+    } else if (this.cachedValue !== query) {
+      this.search = null;
+      this.gitSearch(query);
+    }
+    return this.search;
   }
 }
