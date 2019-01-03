c907657282106057220a62c353815eed9d7a14b0
Niall Riordan <nriordan.dev@gmail.com>
2019-01-03 10:35:28 +0100

    Create a Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +23,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     GitSearchService,
--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { GitSearchService } from '../git-search.service';
 import { GitSearch } from '../git-search';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { AdvancedSearchModel } from '../advanced-search-model';
 
 @Component({
@@ -14,12 +15,19 @@ export class GitSearchComponent implements OnInit {
   searchQuery: string;
   title: string;
   displayQuery: string;
+  form: FormGroup;
+  formControls = {};
 
   constructor(
     private gitSearchService: GitSearchService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {
+    this.modelKeys.forEach((key) => {
+      this.formControls[key] = new FormControl();
+    });
+    this.form = new FormGroup(this.formControls);
+  }
 
   model = new AdvancedSearchModel('', '', '', null, null, '');
   modelKeys = Object.keys(this.model);
@@ -46,19 +54,19 @@ export class GitSearchComponent implements OnInit {
 
   sendQuery = () => {
     this.searchResults = null;
-    const search: string = this.model.q;
+    const search: string = this.form.value['q'];
     let params = '';
-    this.modelKeys.forEach(  (elem) => {
-        if (elem === 'q') {
-            return false;
-        }
-        if (this.model[elem]) {
-            params += '+' + elem + ':' + this.model[elem];
-        }
+    this.modelKeys.forEach((elem) => {
+      if (elem === 'q') {
+        return false;
+      }
+      if (this.form.value[elem]) {
+        params += `+${elem}:${this.form.value[elem]}`;
+      }
     });
     this.searchQuery = search;
     if (params !== '') {
-        this.searchQuery = search + '+' + params;
+      this.searchQuery = `${search}${params}`;
     }
     this.displayQuery = this.searchQuery;
     this.gitSearch();
